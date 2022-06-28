98898c10b0abe1069a85b18f2734a27315bb5f13
thomas jerez <tjerez@outlook.com>
2022-06-28 18:03:22 -0300

    agregamos mensaje por pantallas

---
+++ js/1-holamundo.js
@@ -0,0 +1,25 @@
+//esto es un comentario
+/*esto es 
+un comentario de multiples 
+lineas*/
+
+//mostrar un mensaje en la pantalla o documento
+document.write("<h1>Hola comision 6i</h1>");
+
+//mostrar un mensaje por consola
+console.log("prueba de consola");
+
+//mostrar un mensaje en una ventana emergente
+alert("desde una ventana emergente");
+
+
+
+
+
+
+
+
+
+
+
+
