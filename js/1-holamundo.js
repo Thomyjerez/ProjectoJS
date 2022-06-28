367b017a9c74ce2be9ba715fdfed9e534c51788a
thomas jerez <tjerez@outlook.com>
2022-06-28 18:32:41 -0300

    agregamos valores

--- js/1-holamundo.js
+++ js/1-holamundo.js
@@ -10,8 +10,11 @@ document.write("<h1>Hola comision 6i</h1>");
 console.log("prueba de consola");
 
 //mostrar un mensaje en una ventana emergente
-alert("desde una ventana emergente");
+//alert("desde una ventana emergente");
 
+
+//sumemos dos valores
+document.write("Resultado: " + (12 + 5))
 
 
 
