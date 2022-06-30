f161cd1a6a7aa5586b16e78799e913da88151aad
thomas jerez <tjerez@outlook.com>
2022-06-30 17:49:15 -0300

    agregamos un ejemplo de variables constante

--- js/1-holamundo.js
+++ js/1-holamundo.js
@@ -12,13 +12,22 @@ console.log("prueba de consola");
 //mostrar un mensaje en una ventana emergente
 //alert("desde una ventana emergente");
 
+let nombre = prompt("Ingrese su nombre")
+
+console.log(nombre)
+
+document.write("<p>Hola " + nombre + "</p>");
 
 //sumemos dos valores
 document.write("Resultado: " + (12 + 5))
 
+//ejemplo de constantes
+const url= "<p>https://campus.rollingcodeschool.com/course/view.php?id=104 </p>"
 
+document.write (url)
 
-
+nombre = "thomas"
+document.write("<p>Hola " + nombre + "</p>")
 
 
 
