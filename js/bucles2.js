5eea5700a050a230ba06d1aa0dfb5d849b967ded
thomas jerez <tjerez@outlook.com>
2022-07-07 19:12:40 -0300

    agregue ejercicio switch

---
+++ js/bucles2.js
@@ -0,0 +1,23 @@
+let edad = 18;
+
+do{
+    document.write ("<br> No puede conducir" + edad);
+    edad++;
+
+}while(edad > 18)
+
+
+
+
+
+
+
+
+
+
+
+
+/*while(edad < 18){
+    document.write ("<br> No puede conducir" + edad);
+    edad++;
+}*/
---
+++ js/switch.js
@@ -0,0 +1,66 @@
+let estacion = parseInt(
+  prompt(
+    "Ingrese su estacion del año preferida: 1-verano, 2-otoño, 3-primavera, 4-invierno"
+  )
+);
+
+/*if (estacion == 1) {
+  document.write("Helado");
+} else if (estacion == 2) {
+  document.write("Locro");
+} else if (estacion == 3) {
+  document.write("Chocolate");
+} else if (estacion == 4) {
+  document.write("Cerveza");
+} else {
+  document.write("Ingresaste una opcion erronea");
+}*/
+
+/* switch(opcion){
+    case 1:
+        todas las lineas de codigo en caso que el usuario selecciono 1
+        ...
+        ...
+        ...
+        break;
+    case 2:
+        todas las lineas de codigo en caso que el usuario selecciono 2
+        ...
+        ...
+        ...
+        break;
+    case 3:
+        todas las lineas de codigo en caso que el usuario selecciono 3
+        ...
+        ...
+        ...
+        break;
+        default:
+            todas las lineas que quiero ejecutar en caso que el usuario selecciono una opcion diferente a lo que declare arriba
+        ...                    
+}
+
+*/
+
+switch (estacion) {
+  case 1:
+  case "1":
+  case "verano":
+    document.write("Helado");
+    break;
+
+  case 2:
+    document.write("Locro");
+    break;
+
+  case 3:
+    document.write("Chocolate");
+    break;
+
+  case 4:
+    document.write("Cerveza");
+    break;
+
+  default:
+    document.write("Ingresaste una opcion erronea");
+}
